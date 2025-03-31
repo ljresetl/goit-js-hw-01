9243377ff7a4ec867edc697dae90e648e195604f
jres <ljresetl@gmail.com>
2025-03-31 22:17:27 +0200

    jres

--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,16 @@
+
+
+
 function getElementWidth(content, padding, border) {
-    content = Number.parseFloat(content);
-    padding = Number.parseFloat(padding) * 2;
-    border = Number.parseFloat(border) * 2;
-    return content + border + padding;
+  // Видаляємо 'px' та конвертуємо значення в числа
+  let contentWidth = parseFloat(content);
+  let paddingWidth = parseFloat(padding) * 2;
+  let borderWidth = parseFloat(border) * 2;
+
+  // Оскільки box-sizing: border-box, всі значення входять у підсумкову ширину
+  return contentWidth + paddingWidth + borderWidth;
 }
+
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
 console.log(getElementWidth("200px", "0px", "0px")); // 200
